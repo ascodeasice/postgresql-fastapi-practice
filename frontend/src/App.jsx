ecf7a935948ea14d4a8a90666435d9866e53b202
Leo Wang <ab0988956087@gamil.com>
2023-06-23 19:38:15 +0800

    feat(app.jsx): update routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUpPage from './components/SignUpPage';
 import LoginPage from './components/LoginPage';
 import UserPage from './components/UserPage';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App () {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<SignUpPage />} />
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/sign-up' element={<SignUpPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/user' element={<UserPage />} />
         </Routes>
