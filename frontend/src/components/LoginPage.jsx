f32b324a1c5f5e8ee994a89456e30438ad18bf6e
Leo Wang <ab0988956087@gamil.com>
2023-06-23 19:06:52 +0800

    feat(login-page): finish login-page

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Button, FormControl,  FormErrorMessage,  FormLabel, HStack, Heading, Input,  Text, VStack,Link} from '@chakra-ui/react'
+import { fetchBackend } from '../fetch';
+import { useNavigate } from 'react-router';
+import {Link as ReactRouterLink} from 'react-router-dom'
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMessage ,setErrMessage] = useState('');
+  const navigate=useNavigate();
+
+  const submit = async(e) => {
+    e.preventDefault();
+    const res=await fetchBackend('/login','POST',{
+      username:username,
+      password:password
+    });
+
+    if(res.status===200){
+      window.localStorage.setItem('jwt',res.jwt);
+      navigate('/user');
+    }else{
+      console.error(res);
+      setErrMessage(res.detail)
+    }
+  }
+
   return (
-    <div>LoginPage</div>
-  );
+    <VStack bgColor={'orange.50'} minH={'100vh'}>
+      <form onSubmit={(e)=>submit(e)}>
+      <VStack w={'40vw'}>
+      <Heading>Log In</Heading>
+      <FormControl isRequired>
+      <FormLabel htmlFor='username'>Username</FormLabel>
+      <Input id='username' variant={'filled'} value={username} onChange={(event)=>setUsername(event.target.value)}/>
+      </FormControl>
+      <FormControl isRequired>
+      <FormLabel htmlFor='password'>Password</FormLabel>
+      <Input id='password' variant={'filled'} value={password} onChange={(event)=>setPassword(event.target.value)}/>
+      </FormControl>
+      <Text color={'red.500'}>
+        {errMessage}
+      </Text>
+      <Button colorScheme='blue' type='submit'>Submit</Button>
+      </VStack>
+    </form>
+    <HStack>
+    <Text>Don't have account?</Text>
+    <Link to='/sign-up' color='blue.400' as={ReactRouterLink}>Sign Up</Link>
+    </HStack>
+   </VStack>
+    );
 };
 
 export default LoginPage;
